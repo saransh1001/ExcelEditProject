5bd26e9d7c0017d9fbf66de07d130f8060e2ff10
Saransh Mittal <saransh.mittal@Saranshs-MacBook-Pro.local>
2022-06-28 11:16:33 +0530

    dynamic functionality for cell color & cell border color added

--- src/main/java/com/project/Excel/controllers/excelApplicationController.java
+++ src/main/java/com/project/Excel/controllers/excelApplicationController.java
@@ -9,18 +9,23 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class excelApplicationController {
 
-    @Autowired
-    private excelServiceImplementation excelServiceImplementation;
+        @Autowired
+        private excelServiceImplementation excelServiceImplementation;
 
-
-    public void runExcelApplicationServices(String json, boolean addMacro) {
-        ApplicationContext context= new AnnotationConfigApplicationContext(javaConfig.class);
-       excelServiceImplementation= context.getBean("excelServiceImplementation", com.project.Excel.services.excelServiceImplementation.class);
-        try{
-            // controller is now calling the service
-            this.excelServiceImplementation.getExcelFromData(json,addMacro);
-        } catch (Exception e){
-            e.printStackTrace();
+        public void runExcelApplicationServices(String json, boolean addMacro) {
+            runExcelApplicationServices(json,addMacro,"yellow","green");
         }
-    }
+        public void runExcelApplicationServices(String json, boolean addMacro,String cellColor) {
+            runExcelApplicationServices(json,addMacro,cellColor,"green");
+        }
+        public void runExcelApplicationServices(String json, boolean addMacro,String cellColor,String cellBorderColor) {
+            ApplicationContext context= new AnnotationConfigApplicationContext(javaConfig.class);
+            excelServiceImplementation= context.getBean("excelServiceImplementation", com.project.Excel.services.excelServiceImplementation.class);
+            try{
+                // controller is now calling the service
+                this.excelServiceImplementation.getExcelFromData(json,addMacro,cellColor,cellBorderColor);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
 }
--- src/main/java/com/project/Excel/services/excelService.java
+++ src/main/java/com/project/Excel/services/excelService.java
@@ -6,9 +6,16 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public interface excelService {
-    public void getExcelFromData(String json, boolean addMacro) throws Exception;
+//    public void getExcelFromData(String json, boolean addMacro) throws Exception;
+//    public  void writeDataToExcel(ArrayList<String> headings, JsonArray arr, boolean addMacro) throws Exception;
+//    public void addMacroToExcel(Workbook workbook, VbaModule module, int idx, String dataDir) throws Exception;
+//    public String getMacroCode() throws FileNotFoundException;
+//
+//    public String getMacroAutoSizeCode() throws FileNotFoundException;
+    public void getExcelFromData(String json, boolean addMacro,String cellColor,String cellBorderColor) throws Exception;
     public  void writeDataToExcel(ArrayList<String> headings, JsonArray arr, boolean addMacro) throws Exception;
     public void addMacroToExcel(Workbook workbook, VbaModule module, int idx, String dataDir) throws Exception;
     public String getMacroCode() throws FileNotFoundException;
+    public String getMacroCodeFromFiles(String resource) throws FileNotFoundException;
 
 }
